be7e8ed185780afa0702acf9599dfe872b28b069
fjolivo <fjolivo@gmail.com>
2022-12-02 23:09:38 -0400

    METAVERSUS

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -12,6 +12,16 @@ const Hero = () => (
     initial="hidden"
     whileInView="show"
     viewport={{ once: false, amount: 0.25 }}
+    className={"flex justify-center items-center flex-col relative z=10"}
+    >
+      <div className="flex justify-center items-center flex-col relative z-10">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+        METAVERSE
+        </motion.h1>
+      </div>
+
+  </motion.div>
+
   />
     Hero section
   </section>
