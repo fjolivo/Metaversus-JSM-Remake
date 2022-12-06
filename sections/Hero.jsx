1498471edc0bfaa24353391847cd6e965b6a9437
fjolivo <fjolivo@gmail.com>
2022-12-06 08:12:40 -0400

    Todays update
    Just used `git rm -r --cached some-directory` command to ignore files

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -8,11 +8,11 @@ import {slideIn, staggerContainer, textVariant } from '../utils/motion';
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
   <motion.div
-    variant={staggerContainer}
+    variants={staggerContainer}
     initial="hidden"
     whileInView="show"
     viewport={{ once: false, amount: 0.25 }}
-    className={"flex justify-center items-center flex-col relative z=10"}
+    className={`${styles.innerWidth} mx-auto flex flex-col`}
   >
     <div className="flex justify-center items-center flex-col relative z-10">
       <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
