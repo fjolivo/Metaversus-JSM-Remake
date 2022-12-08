29d1f60f22ca4177014f60a5a8fd753455d18518
fjolivo <fjolivo@gmail.com>
2022-12-07 20:24:37 -0400

    Insight section finished

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -9,7 +9,7 @@ import { InsightCard, TitleText, TypingText } from '../components';
 import { insights } from '../constants';
 
 const Insights = () => (
-  <section  className={`${styles.paddings} realtive z-10`}>
+  <section  className={`${styles.paddings} relative z-10`}>
     <motion.div
       variants={staggerContainer}
       initial="hidden"
@@ -19,10 +19,11 @@ const Insights = () => (
     >
       <TypingText title="| Insight" textStyles="text-center" />
       <TitleText title="Insight about metaverse" textStyles="text-center" />
-      <div className="mt-[50px] flex flex-col gap-[30px]"></div>
+      <div className="mt-[50px] flex flex-col gap-[30px]">
         {insights.map((insight, index) => (
           <InsightCard key={`insight-${index}`} {...insight} index={index + 1} />
         ))}
+      </div>
     </motion.div>
   </section>
 );
